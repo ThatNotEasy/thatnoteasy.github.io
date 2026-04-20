e3f49168d56b82a91bb3a2bd0fd3a26c337ef141
Pari Malam <apid9755@gmail.com>
2026-04-20 18:37:19 +0800

    released

---
+++ assets/js/tailwind-config.js
@@ -0,0 +1,1 @@
+tailwind.config={theme:{extend:{colors:{cyber:{900:'#0a0a1a',800:'#0d0d2b',700:'#12123a',600:'#1a1a4a',500:'#2a2a5a'},neon:{purple:'#a855f7',cyan:'#22d3ee',pink:'#ec4899',blue:'#3b82f6',green:'#10b981',red:'#ef4444',orange:'#f59e0b',yellow:'#eab308'}},fontFamily:{mono:['JetBrains Mono','Fira Code','monospace']}}}};
